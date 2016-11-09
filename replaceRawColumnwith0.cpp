aa9e99fe21606469265c01f76263a9b219f7fb46
pk79 <prakhar79@gmail.com>
2016-11-09 12:45:15 -0800

    Adding replaceRawColumnwith0.cpp

---
+++ replaceRawColumnwith0.cpp
@@ -0,0 +1,16 @@
+/*
+This prgomram wills take an array.
+*/
+
+#include <iostream.h>
+
+class matrix
+{
+    public:
+        int replaceWithZero(char *mat);
+};
+
+int matrix ::replaceWithZero(char *mat)
+{
+    
+}
