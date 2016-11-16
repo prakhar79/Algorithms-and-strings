94734aeb955a73545927b769cf64b600923b8ab9
pk79 <prakhar79@gmail.com>
2016-11-15 21:32:12 -0800

    sorting.cpp

--- sorting.cpp
+++ sorting.cpp
@@ -10,11 +10,41 @@ using namespace std;
 class doSort
 {
     public:
-        void mergeSort(int *arr);
+        void mergeSort(int arr[]);
 };
 
-void doSort :: mergeSort(int *arr)
+/*void doSort :: mergeSort(int arr[],int ref =0)
 {
-    int len = sizeof(arr)/ sizeof(arr[0]);
+    int len = len(arr)/len(arr[0]);
+    int mid;
+    int left[mid],right[len-mid];
+    int i,j=0,k;
+    for (i=0;i<min;i++)
+    {
+        left[i] = arr[j];
+        j++;
+    }
+    for (i=0;i<len-min;i++)
+    {
+        left[i] = arr[j];
+        j++;
+    }
+    mergeSort(left,0);
+    mergeSort(right, ref + mid);
 
-}
+    left = len/2;
+    right = len-left;
+
+    mergeSort(left);
+    mergeSort(right);
+
+    i=0;
+    j=0;
+    k=
+
+} */
+
+void doSort :: mergeSort(int *arr,int len)
+{
+    int mid=len/2;
+    mergeSort(arr + )
