4c5769a3597177ad8e72f0a2751123fb4bc8e74c
pk79 <prakhar79@gmail.com>
2016-11-15 21:19:51 -0800

    Adding sorting.cpp

---
+++ sorting.cpp
@@ -0,0 +1,20 @@
+/**
+ *This program contains implimentation of different sorting alogrythms.
+ */
+
+#include <iostream.h>
+#include <stdio.h>
+
+using namespace std;
+
+class doSort
+{
+    public:
+        void mergeSort(int *arr);
+};
+
+void doSort :: mergeSort(int *arr)
+{
+    int len = sizeof(arr)/ sizeof(arr[0]);
+
+}
